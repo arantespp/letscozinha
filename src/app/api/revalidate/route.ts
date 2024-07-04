802952b4b0bb88e301b117b8339c3f269681913d
Pedro Arantes <arantespp@gmail.com>
2024-07-03 23:47:43 -0300

    fix: add log to api revalidate

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -4,13 +4,13 @@ import { revalidatePath } from 'next/cache';
 const LETS_COZINHA_API_KEY = process.env.LETS_COZINHA_API_KEY;
 
 export async function POST(request: NextRequest) {
-  const headers = request.headers;
-
-  if (headers.get('x-api-key') !== LETS_COZINHA_API_KEY) {
+  if (request.headers.get('x-api-key') !== LETS_COZINHA_API_KEY) {
     return new NextResponse('Unauthorized', { status: 401 });
   }
 
   const { paths } = await request.json();
+
+  console.log('Revalidating paths:', paths);
 
   for (const path of paths) {
     revalidatePath(path);
