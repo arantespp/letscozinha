48d3c27c6e556134873548bf23cffb673300d70f
Pedro Arantes <arantespp@gmail.com>
2024-07-04 00:27:05 -0300

    fix: remove logs

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -4,18 +4,11 @@ import { revalidatePath } from 'next/cache';
 const LETS_COZINHA_API_KEY = process.env.LETS_COZINHA_API_KEY;
 
 export async function POST(request: NextRequest) {
-  console.log('AAAAAAAAA');
-
   if (request.headers.get('x-api-key') !== LETS_COZINHA_API_KEY) {
-    console.log('BBB');
     return new NextResponse('Unauthorized', { status: 401 });
   }
 
-  console.log('CCCCCC');
-
   const body = await request.json();
-
-  console.log('Revalidating paths:', body);
 
   for (const path of body.paths) {
     revalidatePath(path);
