f4daef6476d606d916fad2dce6ac4a209c9f22e8
Pedro Arantes <arantespp@gmail.com>
2024-11-11 17:04:00 -0300

    chore: add logs to test

--- src/methods/getFontData.ts
+++ src/methods/getFontData.ts
@@ -1,7 +1,11 @@
 import { join } from 'node:path';
-import { readFile } from 'node:fs/promises';
+import { readFile, readdir } from 'node:fs/promises';
 
 export const getFontData = async (): Promise<Buffer | ArrayBuffer> => {
+  for (const file of await readdir(join(process.cwd(), 'assets'))) {
+    console.log('file', file);
+  }
+
   const fontDataBuffer = await readFile(
     join(process.cwd(), 'assets', 'PlayfairDisplay-Regular.ttf')
   );
